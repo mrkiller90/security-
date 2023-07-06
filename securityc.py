3f43451a1bf4b80fab79d9f9c5cd79f2dd536950
mrkiller90 <136891488+mrkiller90@users.noreply.github.com>
2023-07-07 01:39:30 +0330

    Add files via upload

--- securityc.py
+++ securityc.py
@@ -8,14 +8,22 @@ while True:
     command = "netstat -anp | grep 'tcp\|udp' | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort"
     p = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output, error = p.communicate()
-    lines = output.decode().split("\n")
-    for line in lines:
-        if line:
-            count, ip = line.strip().split()
-            count = int(count)
-            if count > 2000:
-                delete_command = f"route delete {ip}"
-                delete_p = subprocess.Popen(delete_command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                delete_p.communicate()
+    if output:
+        lines = output.decode().split("\n")
+        for line in lines:
+            if line:
+                count, ip = line.strip().split()
+                count = int(count)
+                if count > 2000:
+                    delete_command = f"route delete {ip}"
+                    delete_p = subprocess.Popen(delete_command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    delete_output, delete_error = delete_p.communicate()
+                    if delete_output:
+                        print(delete_output.decode())
+                    if delete_error:
+                        print(delete_error.decode())
+    else:
+        print("There was an error running the command.")
+
     time.sleep(300)
     os.system("Script Is Running.... Dont Worry ;)")
